94b513d9738780db97fd4ec7b57a46916fad366f
Divyam1999 <grandmasterrocks@gmail.com>
2020-03-01 21:18:22 +0530

    attendance details are now coonected to mysql database

--- Application.py
+++ Application.py
@@ -9,20 +9,22 @@ window.state('zoomed') # This line lets the application have a title bar
 window.title('FaceRec Attandance System')
 tk.Label(window,text='FACE RECOGNITION BASED ATTENDANCE SYSTEM',font=('Arial Bold',35)).place(x=60,y=0)
 
-#Creating a frame that would take input and give notification
-first_frame = tk.LabelFrame(window,text=' Add a new record: ',font=('Lucida',20),padx=10,pady=10)
-first_frame.place(x=40,y=150)
+#Creating 
+first_frame = tk.LabelFrame(window,text=' Add a new record: ',font=('Arial Bold',20),padx=10,pady=10)
+first_frame.place(x=80,y=150)
+
+second_frame = tk.LabelFrame(window,text='Notification',font=('Arial Bold',20),padx=160,pady=10)
+second_frame.place(x=80,y=500)
 
 # defining the controls of the record
-tk.Label(first_frame,text='Enter Name:',font=('Lucia',19)).grid(row=0,column=0,padx=10,pady=10)
+tk.Label(first_frame,text='Enter Roll no:',font=('Lucia',19)).grid(row=0,column=0,padx=10,pady=10)
 roll=tk.Entry(first_frame,width=60)
 roll.grid(row=0,column=1)
-tk.Label(first_frame,text='Enter ID: ',font=('Lucia',19)).grid(row=1,column=0)
+tk.Label(first_frame,text='Enter Name: ',font=('Lucia',19)).grid(row=1,column=0)
 name=tk.Entry(first_frame,width=60)
 name.grid(row=1,column=1)
-tk.Label(first_frame,text='Notification',font=('Lucia',20)).grid(row=2,column=0,padx=10,pady=10)
-notification=tk.Label(first_frame,text='',font=('Lucia',16))
-notification.grid(row=2,column=1,padx=5,pady=10)
+notification=tk.Label(second_frame,text='',font=('Lucia',16),bg='white')
+notification.grid(row=0,column=1)
 
 
 #defining the functions for all buttons
@@ -31,33 +33,36 @@ def clear():
     name.delete(0,'end')
     notification.configure(text='')
 
+
+def Quit():
+    window.destroy()
+
 def addRecord():
     ID=roll.get()
     NAME=name.get()
     message=fr.TakeImage(ID,NAME)
     notification.configure(text=message)
-    return
 
 def trainRecord():
     fr.TrainImage()
     notification.configure(text='Images Trained Sucessfully')
 
 def takeAttendance():
-    fr.TrackImage()
-    notification.configure(text='')
+    message = fr.TrackImage()
+    notification.configure(text=message)
 
 
 #defining a second frame for buttons
-second_frame = tk.LabelFrame(window)
-second_frame.place(x=40,y=400)
+third_frame = tk.LabelFrame(window)
+third_frame.place(x=80,y=400)
 
 
 #making the button to call the functions
-tk.Button(second_frame,text='Clear',fg='white',bg='orange',font=('Lucida Fax',16),command=clear).grid(row=6,column=1,padx=10,pady=20)
-tk.Button(second_frame,text='Add Record',fg='white',bg='blue',font=('Lucida Fax',16),command=addRecord).grid(row=6,column=2,padx=10,pady=20)
-tk.Button(second_frame,text='Train Records',font=('Lucida Fax',16),fg='white',bg='green',command=trainRecord).grid(row=6,column=3,padx=10,pady=10)
-tk.Button(second_frame,text='Take Attandance',font=('Lucida fax',16),fg='white',bg='black',command=takeAttendance).grid(row=6,column=4,padx=10,pady=10)
-tk.Button(second_frame,text='Quit',font=('Lucida fax',16),fg='white',bg='red').grid(row=10,column=7,padx=10,pady=10)
+tk.Button(first_frame,text='Clear',fg='white',bg='orange',font=('Lucida Fax',16),command=clear).grid(row=6,column=0,padx=10,pady=20)
+tk.Button(first_frame,text='Add Record',fg='white',bg='blue',font=('Lucida Fax',16),command=addRecord).grid(row=6,column=1,padx=10,pady=20)
+tk.Button(third_frame,text='Train Records',font=('Lucida Fax',16),fg='white',bg='green',command=trainRecord).grid(row=6,column=3,padx=20,pady=10)
+tk.Button(third_frame,text='Take Attandance',font=('Lucida fax',16),fg='black',bg='Gray',command=takeAttendance).grid(row=6,column=4,padx=20,pady=10)
+tk.Button(third_frame,text='Close',font=('Lucida fax',16),fg='white',bg='red',command=Quit).grid(row=6,column=7,padx=20,pady=10)
 
 
 
@@ -66,8 +71,18 @@ tk.Button(second_frame,text='Quit',font=('Lucida fax',16),fg='white',bg='red').g
 
 #creating a fourth frame to add image
 fourth_frame=tk.LabelFrame(window,width=100,height=100)
-fourth_frame.place(x=850,y=160)
+fourth_frame.place(x=670,y=170)
 img=ImageTk.PhotoImage(Image.open('GuiFiles/Face-ID.jpg'))
 tk.Label(fourth_frame,image=img,width=400,height=400).grid(column=0,row=0)
+
+# creating a fifth frame 
+fifth_frame = tk.LabelFrame(window,text='View Reports',font=('Arial Bold',20),width=100,height=100,padx=10,pady=10)
+fifth_frame.place(x=1100,y=150)
+
+# making report buttons
+tk.Button(fifth_frame,text='Late Report',fg='white',bg='orange',font=('Lucida Fax',16)).pack(padx=10,pady=20,fill='x')
+tk.Button(fifth_frame,text='Weekly Report',fg='white',bg='purple',font=('Lucida Fax',16)).pack(padx=10,pady=20,fill='x')
+tk.Button(fifth_frame,text='Monthly Report',fg='white',bg='black',font=('Lucida Fax',16)).pack(padx=10,pady=20,fill='x')
+tk.Button(fifth_frame,text='Annual Report',fg='white',bg='brown',font=('Lucida Fax',16)).pack(padx=10,pady=20,fill='x')
 
 window.mainloop()
--- Face_Recognition.py
+++ Face_Recognition.py
@@ -2,13 +2,15 @@ import cv2
 import numpy as np 
 import os
 from os import listdir
+from pandas.io import sql
 import pandas as pd
 from PIL import Image, ImageTk
 import datetime
 import time
+import sqlalchemy
 
+connection = sqlalchemy.create_engine('mysql+pymysql://root:open5920@localhost/facerecdb')
 face_classifier = cv2.CascadeClassifier('HaarCascade\haarcascade_frontalface_default.xml')
-
 
 def image_and_lables_extractor(path):
     imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
@@ -44,9 +46,7 @@ def TakeImage(ID,NAME):
                 if cv2.waitKey(1)==13 or count==100:
                     cap.release()
                     cv2.destroyAllWindows()
-                    return 'Collecting Images Complete! Please train the record'
-
-
+                    return 'Image Samples Collected!'
 
 
 def TrainImage():
@@ -61,9 +61,10 @@ def TrackImage():
     recognizer = cv2.face.LBPHFaceRecognizer_create()#cv2.createLBPHFaceRecognizer()
     recognizer.read("TrainingData.yml")
     df=pd.read_csv("StudentDetails.csv")
+    df = pd.read_sql('SELECT userid,name FROM details',con=connection)
     cam = cv2.VideoCapture(0)
     font = cv2.FONT_HERSHEY_SIMPLEX
-    col_names =  ['Id','Name','Date','Time']
+    col_names =  ['userid','name','date','time']
     attendance = pd.DataFrame(columns = col_names)
     while True:
         ret, im =cam.read()
@@ -76,25 +77,24 @@ def TrackImage():
                 ts = time.time()      
                 date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
                 timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
-                aa=df.loc[df['Id'] == Id]['Name'].values
+                aa=df.loc[df['userid'] == Id]['name'].values
                 tt=str(Id)+"-"+aa
-                attendance.loc[len(attendance)] = [Id,aa,date,timeStamp]
+                attendance.loc[len(attendance)] = [Id,aa[0],date,timeStamp]
             else:
                 Id='Unknown'                
                 tt=str(Id)  
             if(conf > 75):
                 noOfFile=len(os.listdir("ImagesUnknown"))+1
                 cv2.imwrite("ImagesUnknown\Image"+str(noOfFile) + ".jpg", im[y:y+h,x:x+w])            
-            cv2.putText(im,str(tt),(x,y+h), font, 1,(255,255,255),2)        
-        attendance=attendance.drop_duplicates(subset=['Id'],keep='first')    
+            cv2.putText(im,str(tt),(x,y+h), font, 1,(255,255,255),2)      
+        attendance=attendance.drop_duplicates(subset=['userid'],keep='first')    
         cv2.imshow('Taking Your Attendance',im) 
         if (cv2.waitKey(1)==13):
             break
-    ts = time.time()      
-    date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
-    timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
-    Hour,Minute,Second=timeStamp.split(":")
-    fileName="Attendance/Attendance"+date+"_"+Hour+"-"+Minute+"-"+Second+".csv"
-    attendance.to_csv(fileName,index=False)
     cam.release()
     cv2.destroyAllWindows()
+    attendance.to_sql(con=connection,name='attendance',if_exists='append',index=False)
+    if attendance.empty:
+        return 'No Known record found!'
+    else:
+        return 'Attendance Taken!'
